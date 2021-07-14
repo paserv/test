ab182580d7197d4701200d0a1899bbb417184955
Paolo Servillo <servill7@posteitaliane.it>
2021-07-14 17:37:13 +0200

    with dependency

--- __tests__/Example.test.ts
+++ __tests__/Example.test.ts
@@ -1,5 +1,10 @@
 import { Greeter } from '../src/Greeter';
+import { Common } from 'common';
 
 test('My Greeter', () => {
-  expect(Greeter.getVersion()).toBe('Main version');
+    expect(Greeter.getVersion()).toBe('Main version');
+});
+
+test('Common', () => {
+    expect(Common.getVersion()).toBe('Branch version');
 });
--- src/Greeter.ts
+++ src/Greeter.ts
@@ -4,6 +4,11 @@ class Greeter {
   static getVersion(): string {
     return 'Main version';
   }
+
+  static testCommon(): string {
+    return 'Main version';
+  }
+
 }
 
 export { Greeter };
